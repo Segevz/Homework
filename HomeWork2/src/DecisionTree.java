19bb6e0d164df2e33662e93227248452a8c7db9d
Segev Fogel <segev.fogel@gmail.com>
2018-04-16 11:34:45 +0300

    added nadav changes

--- HomeWork2/src/DecisionTree.java
+++ HomeWork2/src/DecisionTree.java
@@ -1,7 +1,9 @@
 import weka.classifiers.Classifier;
+import weka.core.Attribute;
 import weka.core.Capabilities;
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.filters.unsupervised.instance.RemoveWithValues;
 
 class Node {
 	Node[] children;
@@ -20,18 +22,71 @@ public class DecisionTree implements Classifier {
 	}
 
 	private void buildTree(Instances data) {
+		int entropyError, giniError
+		Node rootEntropy = new Node();
+		Node rootGini = new Node();
+		buildEntropyTree(data, rootEntropy);
+		buildGiniTree(data, rootGini);
+		rootNode = rootEntropy;
+		entropyError = calcAvgError()
 
 	}
 
-	private Node buildGiniTree(Instances data) {
-		return null;
+	private void buildGiniTree(Instances data, Node current) {
+		if (!checkDichotomy(data)){
+			return;
+		}
+		int bestAttribute = -1;
+		double maxGain = 0, currentGain;
+		for (int i = 0; i < data.numAttributes() - 1; i++) {
+			currentGain = calcGiniGain(data, i);
+			if (currentGain > maxGain) {
+				maxGain = currentGain;
+				bestAttribute = i;
+			}
+		}
+		current.attributeIndex = bestAttribute;
+		makeChildren(data, current, true);
 	}
 
-	private Node buildEntropyTree(Instances data) {
-		Node root = new Node();
 
+	private void buildEntropyTree(Instances data, Node current) {
+		if (!checkDichotomy(data)){
+			return;
+		}
+		int bestAttribute = -1;
+		double maxGain = 0, currentGain;
+		for (int i = 0; i < data.numAttributes() - 1; i++) {
+			currentGain = calcInformationGain(data, i);
+			if (currentGain > maxGain) {
+				maxGain = currentGain;
+				bestAttribute = i;
+			}
+		}
+		current.attributeIndex = bestAttribute;
+		makeChildren(data, current, false);
+	}
 
-		return root;
+	private boolean checkDichotomy(Instances data) {
+		return false;
+	}
+
+	private void makeChildren(Instances data,Node parent, boolean impurityMeasure){
+		if (parent.attributeIndex == -1){
+			return;
+		}
+		Instances[] subsetsByAttribute = splitDataByAttribute(data, parent.attributeIndex);
+		for (int i = 0; i < data.attribute(parent.attributeIndex).numValues(); i++) {
+			parent.children[i] = new Node();
+			parent.children[i].parent = parent;
+			parent.children[i].returnValue = calcReturnValue(subsetsByAttribute[i]);
+			parent.children[i].dataSize = subsetsByAttribute[i].size();
+			if (impurityMeasure) {
+				buildGiniTree(subsetsByAttribute[i], parent.children[i]);
+			}else {
+				buildEntropyTree(subsetsByAttribute[i], parent.children[i]);
+			}
+		}
 	}
 
 	private double calcInformationGain(Instances data, int attributeIndex){
@@ -46,12 +101,87 @@ public class DecisionTree implements Classifier {
 		return 1 - (p1 * p1 + p2 * p2);
 	}
 
-	private double calcEntropy(double p1, double p2){
+	private void findBestInfoGain (Node node, Instances data){
+		double maxGain = 0, currentGain;
+		int bestAttributeIndex = 0;
+		double firstEntropy = calcFirstEntropy(data);
+		for (int i = 0; i < data.numAttributes() - 1; i++) {
+			//TODO: write a function for calculating entropy for root node
+			currentGain = calcInfoGain(data, firstEntropy, i);
+			if (currentGain > maxGain){
+				maxGain = currentGain;
+				bestAttributeIndex = i;
+			}
+		}
+		node.attributeIndex = bestAttributeIndex;
+	}
+
+	private double calcInfoGain(Instances data, double entropy, int indexAttribute) {
+		Instances subset;
+		Attribute attribute = data.attribute(indexAttribute);
+		double gain = entropy;
+		for (int i = 0; i < attribute.numValues(); i++) {
+			//TODO: fix split function
+			subset = splitDataByAttribute(data, indexAttribute, i);
+			gain -= (subset.size() / data.size()) * calcEntropy(subset, attribute.value(i), indexAttribute);
+		}
+		return gain;
+	}
+	private double calcEntropy(Instances data, String attributeValue, int attributeIndex) {
+		int dataSize = data.size();
+		int classIndex = data.classIndex();
+		double hasCancer = 0, noCancer = 0;
+		Instance currentInstace;
+		for (int i = 0; i < dataSize; i++) {
+			currentInstace = data.get(i);
+			if (currentInstace.stringValue(attributeIndex).equals(attributeValue)) {
+				if (currentInstace.stringValue(classIndex).equals("recurrence-events")) {
+					hasCancer++;
+				} else {
+					noCancer++;
+				}
+			}
+		}
+		return entropyFormula(hasCancer / dataSize, noCancer / dataSize);
+	}
+
+	private double entropyFormula(double p1, double p2){
 		return -(p1 * Math.log(p1)/Math.log(2) + p2 * Math.log(p2)/Math.log(2));
 	}
 
 	private Instances[] splitDataByAttribute(Instances data, int attributeIndex) {
 		return null;
+	}
+
+	private Instances[] splitData(Instances data, int attributeIndex) {
+		RemoveWithValues filter = new RemoveWithValues();
+		String valueIndex = "";
+		String[] options = new String[5];
+		Instances[] splitData = new Instances[data.attribute(attributeIndex).numValues()];
+		options[0] = "-C";   // Choose attribute to be used for selection
+		options[1] =Integer.toString(attributeIndex); // Attribute number
+		options[2] = "-L";
+		options[4] = "-V";
+
+		for (int i = 0; i < data.attribute(attributeIndex).numValues(); i++) {
+			options[3] = Integer.toString(i);
+			try {
+				filter.setOptions(options);
+				filter.setInputFormat(data);
+				splitData[i] = filter.useFilter(data, filter);
+			} catch (Exception e) {
+				System.err.println("Error : " + e);
+			}
+		}
+		return splitData;
+	}
+
+	private String makeRange(int n) {
+		String result = "0";
+		for (int i = 0; i < n; i++) {
+			result += "," + Integer.toString(i);
+		}
+		return result;
 	}
 
 
