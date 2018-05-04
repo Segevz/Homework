97fbda6d85dd6b2556dbebf245a35037e05aa935
Nadav Miran <miran.nadav@gmail.com>
2018-05-04 13:58:11 +0300

    small main updates

--- HomeWork3/src/HomeWork3/MainHW3.java
+++ HomeWork3/src/HomeWork3/MainHW3.java
@@ -10,66 +10,71 @@ import java.io.IOException;
 public class MainHW3 {
 
 
-	public static BufferedReader readDataFile(String filename) {
-		BufferedReader inputReader = null;
+    public static BufferedReader readDataFile(String filename) {
+        BufferedReader inputReader = null;
 
-		try {
-			inputReader = new BufferedReader(new FileReader(filename));
-		} catch (FileNotFoundException ex) {
-			System.err.println("File not found: " + filename);
-		}
+        try {
+            inputReader = new BufferedReader(new FileReader(filename));
+        } catch (FileNotFoundException ex) {
+            System.err.println("File not found: " + filename);
+        }
 
-		return inputReader;
-	}
+        return inputReader;
+    }
 
-	public static Instances loadData(String fileName) throws IOException {
-		BufferedReader datafile = readDataFile(fileName);
-		Instances data = new Instances(datafile);
-		data.setClassIndex(data.numAttributes() - 1);
-		return data;
-	}
+    public static Instances loadData(String fileName) throws IOException {
+        BufferedReader datafile = readDataFile(fileName);
+        Instances data = new Instances(datafile);
+        data.setClassIndex(data.numAttributes() - 1);
+        return data;
+    }
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         //TODO: complete the Main method
-		int [] k_values = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-		int [] Lp_distance = {1,2,3, Integer.MAX_VALUE};
-		Knn.WeightingScheme[] schemes = {Knn.WeightingScheme.Weighted, Knn.WeightingScheme.Uniform};
-		int XFolds = 10;
-		String[] dataset = {"original", "scaled"};
-		Instances data = loadData("auto_price.txt");
-		Instances scaledData = FeatureScaler.scaleData(data);
+        int[] k_values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        int[] Lp_distance = {1, 2, 3, Integer.MAX_VALUE};
+        int XFolds = 10;
 
-		Knn knn = new Knn ();
-		knn.buildClassifier(data);
-		double bestError = Double.MAX_VALUE;
-		double currentError;
-		int bestK=0;
-		int bestLP=0;
-		Knn.WeightingScheme majorityFunction = null;
-		knn.setDistanceMethod(Knn.DistanceCheck.Regular);
+        Knn.DistanceCheck[] distanceChecks = {Knn.DistanceCheck.Regular, Knn.DistanceCheck.Efficient};
+        Knn.WeightingScheme[] schemes = {Knn.WeightingScheme.Weighted, Knn.WeightingScheme.Uniform};
+        String[] dataset = {"original", "scaled"};
+        Instances data = loadData("auto_price.txt");
+        data.setClassIndex(data.numAttributes() - 1);
+        int[] x_Folds = {data.size(), 50, 10, 5, 3};
 
-		for (int i = 1; i < k_values.length; i++) {
-			knn.setK(k_values[i]);
-			for (int j = 0; j < Lp_distance.length; j++) {
-				knn.setLp(Lp_distance[j]);
-				for (int k = 0; k < schemes.length; k++) {
-					knn.setWeightingScheme(schemes[k]);
+        Instances scaledData = FeatureScaler.scaleData(data);
+        scaledData.setClassIndex(scaledData.numAttributes() - 1);
 
-					currentError = knn.crossValidationError(scaledData, XFolds);
-					if (currentError < bestError){
-						bestError = currentError;
-						bestK = k_values[i];
-						bestLP = Lp_distance[j];
-						majorityFunction = schemes[k];
-					}
-				}
-			}
-		}
-		System.out.println("Best K is: " + bestK);
-		System.out.println("Best LP is: " + bestLP);
-		System.out.println("Best majorityFunction is: " + majorityFunction);
-		System.out.println("the error is " + bestError);
+        Knn knn = new Knn();
+        knn.buildClassifier(data);
+        double bestError = Double.MAX_VALUE;
+        double currentError;
+        int bestK = 0;
+        int bestLP = 0;
+        Knn.WeightingScheme majorityFunction = null;
+        knn.setDistanceMethod(Knn.DistanceCheck.Regular);
 
-	}
+        for (int i = 1; i < k_values.length; i++) {
+            knn.setK(k_values[i]);
+            for (int j = 0; j < Lp_distance.length; j++) {
+                knn.setLp(Lp_distance[j]);
+                for (int k = 0; k < schemes.length; k++) {
+                    knn.setWeightingScheme(schemes[k]);
 
+                    currentError = knn.crossValidationError(scaledData, XFolds);
+                    if (currentError < bestError) {
+                        bestError = currentError;
+                        bestK = k_values[i];
+                        bestLP = Lp_distance[j];
+                        majorityFunction = schemes[k];
+                    }
+                }
+            }
+        }
+        System.out.println("Best K is: " + bestK);
+        System.out.println("Best LP is: " + bestLP);
+        System.out.println("Best majorityFunction is: " + majorityFunction);
+        System.out.println("the error is " + bestError);
+
+    }
 }
